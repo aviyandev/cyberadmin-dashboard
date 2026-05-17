2e45ff9249e896a0025ed3fbf94d8ae8862bf637
CyberAdmin Team <cyberadmin@example.com>
2026-05-17 12:27:40 +0530

    Fix: Register Fortify view responses in service provider

--- src/CyberAdminServiceProvider.php
+++ src/CyberAdminServiceProvider.php
@@ -4,6 +4,7 @@ namespace CyberAdmin\Dashboard;
 
 use Illuminate\Support\ServiceProvider;
 use Livewire\Livewire;
+use Laravel\Fortify\Fortify;
 
 class CyberAdminServiceProvider extends ServiceProvider
 {
@@ -18,13 +19,48 @@ class CyberAdminServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->loadViewsFrom(__DIR__ . '/View', 'cyberadmin');
+        $this->loadViewsFrom(__DIR__ . '/Stubs/views', 'cyberadmin-auth');
         $this->loadRoutesFrom(__DIR__ . '/Routes/web.php');
         $this->loadTranslationsFrom(__DIR__ . '/Lang', 'tactical');
 
+        $this->registerFortifyViews();
         $this->registerLivewireComponents();
         $this->registerPublishing();
         $this->registerMiddleware();
         $this->registerCommands();
+    }
+
+    protected function registerFortifyViews()
+    {
+        if (class_exists(Fortify::class)) {
+            Fortify::loginView(function () {
+                if (view()->exists('auth.login')) {
+                    return view('auth.login');
+                }
+                return view('cyberadmin-auth::auth.login');
+            });
+
+            Fortify::registerView(function () {
+                if (view()->exists('auth.register')) {
+                    return view('auth.register');
+                }
+                return view('cyberadmin-auth::auth.register');
+            });
+
+            Fortify::requestPasswordResetLinkView(function () {
+                if (view()->exists('auth.forgot-password')) {
+                    return view('auth.forgot-password');
+                }
+                return view('cyberadmin-auth::auth.forgot-password');
+            });
+
+            Fortify::resetPasswordView(function ($request) {
+                if (view()->exists('auth.reset-password')) {
+                    return view('auth.reset-password', ['request' => $request]);
+                }
+                return view('cyberadmin-auth::auth.reset-password', ['request' => $request]);
+            });
+        }
     }
 
     protected function registerLivewireComponents()
@@ -53,6 +89,10 @@ class CyberAdminServiceProvider extends ServiceProvider
             $this->publishes([
                 __DIR__ . '/Database/Migrations/' => database_path('migrations'),
             ], 'cyberadmin-migrations');
+
+            $this->publishes([
+                __DIR__ . '/Stubs/views/auth' => resource_path('views/auth'),
+            ], 'cyberadmin-auth-views');
         }
     }
 
